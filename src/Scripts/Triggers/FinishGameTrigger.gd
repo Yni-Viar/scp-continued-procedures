41013a5c3252958350b1f998be10830145fe5434
Yni-Viar <81305791+Yni-Viar@users.noreply.github.com>
2025-08-07 12:02:58 +0300

    Introduce Entrance Sub-zone.
    Warning,some rounds it will be impossible to win, due to SCP-347 task trigger was moved to new sub-zone, which is currently NOT connected with the rest of facility.

--- src/Scripts/Triggers/FinishGameTrigger.gd
+++ src/Scripts/Triggers/FinishGameTrigger.gd
@@ -1,11 +1,12 @@
 extends Area3D
 
+@export var detect_scp347_with_mtf: bool = true
 var mtf_guarding: int = 0
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is MovableNpc:
 		if body.puppet_class.puppet_class_name == "SCP-347":
-			if get_tree().get_node_count_in_group("MobileTaskForce") > 0 && mtf_guarding > 0:
+			if get_tree().get_node_count_in_group("MobileTaskForce") > 0 && mtf_guarding > 0 && detect_scp347_with_mtf:
 				get_tree().root.get_node("Game/FoundationTask").do_task("task_347")
 				body.queue_free()
 				get_tree().root.get_node("Game").despawn_wave(0)
--- src/Scripts/Triggers/IntruderCheckTrigger.gd
+++ src/Scripts/Triggers/IntruderCheckTrigger.gd
@@ -1,14 +1,15 @@
 extends Area3D
 
-
+@export var detect_scp347: bool = true
+@export var detect_CI:bool = true
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is MovableNpc:
-		if body.puppet_class.puppet_class_name == "SCP-347":
+		if body.puppet_class.puppet_class_name == "SCP-347" && detect_scp347:
 			body.get_node("PlayerModel").get_child(0).infrared_visibility = true
 			if get_tree().get_node_count_in_group("MobileTaskForce") == 0 || get_parent().get_node("FinishGame").mtf_guarding == 0:
 				get_tree().root.get_node("Game/FoundationTask").trigger_event(2, load("res://Scripts/TaskSystem/Tasks/Scp347EmergencyTask.tres"))
-		if body.fraction == 0 && body.puppet_class.team == 3:
+		if body.fraction == 0 && body.puppet_class.team == 3 && detect_CI:
 			if !get_tree().root.get_node("Game/FoundationTask").has_task("task_ci"):
 				get_tree().root.get_node("Game/FoundationTask").trigger_event(2, load("res://Scripts/TaskSystem/Tasks/CIEmergencyTask.tres"))
 				get_tree().root.get_node("Game/UI/HBoxContainer/CallMtfButton").show()
@@ -16,7 +17,7 @@ func _on_body_entered(body: Node3D) -> void:
 
 func _on_body_exited(body: Node3D) -> void:
 	if body is MovableNpc:
-		if body.puppet_class.puppet_class_name == "SCP-347":
+		if body.puppet_class.puppet_class_name == "SCP-347" && detect_scp347:
 			body.get_node("PlayerModel").get_child(0).infrared_visibility = false
 			if get_tree().get_node_count_in_group("MobileTaskForce") == 0 || get_parent().get_node("FinishGame").mtf_guarding == 0:
 				get_tree().root.get_node("Game/FoundationTask").trigger_event(0)
