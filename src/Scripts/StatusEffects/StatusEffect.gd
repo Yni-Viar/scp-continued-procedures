e2e1b187f1a7ef094695808213c97f524d25fdb6
Yni-Viar <yniviar@outlook.com>
2025-11-04 16:53:57 +0300

    W.I.P SCP-067
    - Added items: SCP-067 and DocShock - 067 result.
    - Optimized new models' HLOD.
    - Removed unused parameters in StatusEffect resource.

--- src/Scripts/StatusEffects/StatusEffect.gd
+++ src/Scripts/StatusEffects/StatusEffect.gd
@@ -4,8 +4,6 @@ class_name StatusEffect
 ## Created by Yni, licensed under MIT License
 
 @export var effect_name: String = ""
-@export var strength: float = 0.0
-@export var duration: float = 0.0
 @export var start_command: Array[StatusEffectCommand]
 @export var update_command: Array[StatusEffectCommand]
 @export var destroy_command: Array[StatusEffectCommand]
