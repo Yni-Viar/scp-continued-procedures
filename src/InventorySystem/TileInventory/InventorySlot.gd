764e4627c3c5db035a07dbe028d62ca865b812bd
Yni-Viar <yniviar@outlook.com>
2025-11-06 12:02:01 +0300

    Fix duplicate item click on Android

--- src/InventorySystem/TileInventory/InventorySlot.gd
+++ src/InventorySystem/TileInventory/InventorySlot.gd
@@ -6,6 +6,7 @@ class_name InventorySlot
 ## Item ID
 @export var item_id: int
 var mouse_inside: bool = false
+var item_use_cooldown: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,10 +15,10 @@ func _ready():
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.double_click && get_parent().get_parent().visible && mouse_inside:
-			get_parent().use_item(self)
+			use_item()
 	if event is InputEventScreenTouch:
 		if event.double_tap && get_parent().get_parent().visible && mouse_inside:
-			get_parent().use_item(self)
+			use_item()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -35,3 +36,10 @@ func _inside():
 
 func _outside():
 	mouse_inside = false
+
+func use_item():
+	if !item_use_cooldown:
+		get_parent().use_item(self)
+		item_use_cooldown = true
+		await get_tree().create_timer(0.5).timeout
+		item_use_cooldown = false
