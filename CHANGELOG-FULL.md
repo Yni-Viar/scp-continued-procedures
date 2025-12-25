50456a8ddd3eef1c54237930cbfaf3e884ae4724
Yni-Viar <yniviar@outlook.com>
2025-12-25 15:16:00 +0300

    Update map generator to v10.0

--- CHANGELOG-FULL.md
+++ CHANGELOG-FULL.md
@@ -23,6 +23,7 @@ For plan, check [features changelog](./CHANGELOG-UPCOMING.md)
 - [ ] Remove item shader in favor of 4.5 stencil feature.
 #### Misc changes
 - [x] \[Clutter system\] Ragdolls are now affected to Clutter System.
+- [x] \[Map Generator\] Updated to 10.0.0
 
 ## v5.6.1 (2025.12.23)
 - \[Clutter system\] Use clutter system for Class-D pumpkin, instead of using specific script for handling that.
--- src/MapGen/MapGenRoom.gd
+++ src/MapGen/MapGenRoom.gd
@@ -2,11 +2,19 @@
 extends Resource
 class_name MapGenRoom
 
+enum DoubleRoomTypes {NONE, ROOM2D = 2, ROOM2CD = 3, ROOM3D = 4, ROOM4D = 5}
+
+enum DoubleRoomPosition {UP, DOWN, LEFT, RIGHT}
+
 @export var name: String
 @export var prefab: PackedScene
+## RIGHT for most rooms, room2c RIGHT
 @export var icon_0_degrees: Texture2D
+## UP for most rooms, room2c DOWN
 @export var icon_90_degrees: Texture2D
+## LEFT for most rooms, room2c LEFT
 @export var icon_180_degrees: Texture2D
+## DOWN for most rooms, room2c UP
 @export var icon_270_degrees: Texture2D
 @export_range(1, 100) var spawn_chance: float = 20
 ## Added in mapgen v8. The default value, -1 means,
@@ -18,5 +26,6 @@ class_name MapGenRoom
 @export var guaranteed_spawn: bool = false
 ## Added in mapgen v9. Requires "Double room support" to be enabled.
 @export_group("Double rooms")
-## Which room can be connected with this room.
-@export var double_room_ids: Array[int] = []
+@export var double_room_position: DoubleRoomPosition = DoubleRoomPosition.UP
+## Room shape for double room generation
+@export var double_room_shape: DoubleRoomTypes = DoubleRoomTypes.NONE
